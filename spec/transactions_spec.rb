5d6da6c91376655cc9ca127bfba89cb3dd5d9aeb
Alfie Darko <me@alfiedarko.co.uk>
2018-04-25 21:46:54 +0100

    passing rspec test for Transactions > has a history property that is an empty array

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -1,7 +1,24 @@
 require 'transactions.rb'
 
 describe Transactions do
+
+  before(:all) do
+    transactions = Transactions.new
+  end
+
   it 'has a history property that is an array of zero' do
     expect(subject.history).to eq []
   end
+
+  # it 'adds transactions to the history property' do
+  # balance = double('balance')
+
+  #   allow(balance).to receive(:holdings)
+  #   allow(balance).to receive(:credit) do |args|
+  #     allow(balance).to receive(:holdings) {args}
+  #   end
+  #
+  #   balance.credit(100)
+  #   puts balance.holdings
+  # end
 end
