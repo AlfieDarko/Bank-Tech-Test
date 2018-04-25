7aa69eaee3822b006afbbfd63db1dbe854e255c3
Alfie Darko <me@alfiedarko.co.uk>
2018-04-25 17:13:00 +0100

    failing test for Balance .debit takes away money form the holdings property

--- spec/balance_spec.rb
+++ spec/balance_spec.rb
@@ -1,6 +1,10 @@
 require 'balance.rb'
 
 describe Balance do
+  before(:all) do
+    balance = Balance.new
+  end
+
   it 'has a holdings of zero' do
     expect(subject.holdings).to eq 0
   end
@@ -9,6 +13,15 @@ describe Balance do
     it 'adds money to the holdings property' do
       subject.credit(100.00)
       expect(subject.holdings).to eq 100.00
+    end
+  end
+
+  describe '.debit' do
+    it 'takes away money from the holdings property' do
+      subject.credit(300.00)
+
+      subject.withdraw(100.00)
+      expect(subject.holdings).to eq 200.00
     end
   end
 end
