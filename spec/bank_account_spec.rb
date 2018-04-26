7a6cc84a6318bdfd7e0ec571255800b4c537d26b
Alfie Darko <me@alfiedarko.co.uk>
2018-04-26 11:54:51 +0100

    passing rspec test for BankAccount> it initiates with a balance properrty

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -4,4 +4,17 @@ describe BankAccount do
   it 'initializes with balance class' do
     expect(subject.balance).to be_instance_of Balance
   end
+
+  describe '.show_balance' do
+    it 'returns the balance of the account' do
+      expect(subject.show_balance).to eq 0
+    end
+  end
+  # describe '.deposit' do
+  #   it 'deposits money into balance' do
+  #     subject.deposit(100)
+  #
+  #     expect(subject.).not_to eq object
+  #   end
+  # end
 end
